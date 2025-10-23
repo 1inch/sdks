5d259fe8641e7cdeb7c620a689e01921ca7486bd
Vladimir Borovik <v.borovik@1inch.io>
2025-10-23 18:13:00 +0300

    feat: setup e2e tests

---
+++ typescript/aqua/tests/default-strategies.spec.ts
@@ -0,0 +1,10 @@
+describe('Default Strategies', () => {
+
+  beforeAll(async () => {
+    // Setup code here
+  });
+
+  test('should be able to create a default strategy', () => {
+    expect(1).toBeDefined();
+  });
+})
