6ba8cd4e16a5e1601454638ba12130d9b3b20ab7
rharutyunyan <rado9408@gmail.com>
2025-11-11 16:27:07 +0400

    feat: architecture change

--- typescript/eslint.config.mjs
+++ typescript/eslint.config.mjs
@@ -3,6 +3,17 @@ import oneInchEslintConfig from '@1inch/eslint-config'
 export default [
   ...oneInchEslintConfig,
   {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: ['typescript/tsconfig.json', 'typescript/*/tsconfig.json'],
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs', '.mts', '.json'],
+        },
+      },
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'error',
