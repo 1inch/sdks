8b1a70485f5d0efb0cabb0768f3bcce3e92bc746
Vladimir Borovik <v.borovik@1inch.io>
2025-11-16 19:20:46 +0300

    update addresses

--- typescript/swap-vm/src/swap-vm-contract/constants.ts
+++ typescript/swap-vm/src/swap-vm-contract/constants.ts
@@ -1,20 +1,22 @@
 import { Address, NetworkEnum } from '@1inch/sdk-core'
 
 /**
- * SwapVM contract addresses by chain ID
+ * AquaSwapVM contract addresses by chain ID
+ * These addresses supports only AQUA instructions set
+ *
+ * @see "../swap-vm/programs/aqua-program-builder"
  */
-export const SWAP_VM_CONTRACT_ADDRESSES: Record<NetworkEnum, Address> = {
-  [NetworkEnum.ETHEREUM]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.BINANCE]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.POLYGON]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.ARBITRUM]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.AVALANCHE]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.GNOSIS]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.COINBASE]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.OPTIMISM]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.FANTOM]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.ZKSYNC]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.LINEA]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.UNICHAIN]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
-  [NetworkEnum.SONIC]: new Address('0x11d305af1691D3aca504f6216532675f7Dd07D11'),
+export const AQUA_SWAP_VM_CONTRACT_ADDRESSES: Record<NetworkEnum, Address> = {
+  [NetworkEnum.ETHEREUM]: new Address('0x1384a4d41235bc579e5b28355da64bf1287abdac'),
+  [NetworkEnum.BINANCE]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.POLYGON]: new Address('0xaa64d89e264455ea9eff7416b58cae3f6d84ceb5'),
+  [NetworkEnum.ARBITRUM]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.AVALANCHE]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.GNOSIS]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.COINBASE]: new Address('0xcc4fee4ff03daf14e8ba6c71910b4e078f3d1c6b'),
+  [NetworkEnum.OPTIMISM]: new Address('0xaa64d89e264455ea9eff7416b58cae3f6d84ceb5'),
+  [NetworkEnum.ZKSYNC]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.LINEA]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.UNICHAIN]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
+  [NetworkEnum.SONIC]: new Address('0x0d5c0881cba1a88a6ebf90e89a25ecd2506bb967'),
 }
