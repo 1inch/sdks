ad742adeee6ac34201e1436dcdc05ef79607b746
Kirill Kuznetcov <krboktv@gmail.com>
2026-04-15 19:05:37 +0400

    fix(taker-traits): set HAS_PRE_TRANSFER_IN_CALLBACK_BIT_FLAG and HAS_PRE_TRANSFER_OUT_CALLBACK_BIT_FLAG even in case data is empty or data is not empty

--- typescript/swap-vm/src/swap-vm/taker-traits.ts
+++ typescript/swap-vm/src/swap-vm/taker-traits.ts
@@ -305,11 +305,11 @@ export class TakerTraits {
     flags = flags.setBit(TakerTraits.SHOULD_UNWRAP_BIT_FLAG, this.shouldUnwrap)
     flags = flags.setBit(
       TakerTraits.HAS_PRE_TRANSFER_IN_CALLBACK_BIT_FLAG,
-      this.preTransferInCallbackEnabled,
+      this.preTransferInCallbackEnabled || !this.preTransferInCallbackData.isEmpty(),
     )
     flags = flags.setBit(
       TakerTraits.HAS_PRE_TRANSFER_OUT_CALLBACK_BIT_FLAG,
-      this.preTransferOutCallbackEnabled,
+      this.preTransferOutCallbackEnabled || !this.preTransferOutCallbackData.isEmpty(),
     )
     flags = flags.setBit(TakerTraits.IS_STRICT_THRESHOLD_BIT_FLAG, this.strictThreshold)
     flags = flags.setBit(
