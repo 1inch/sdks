23db9461977747ab8b861f210360f082ea73f409
Artem Vorobev <artall64@gmail.com>
2026-04-16 19:08:58 +0300

    bober(sprint-1): fix CallInfo type-only re-export in aqua and swap-vm (#58)

--- typescript/aqua/src/index.ts
+++ typescript/aqua/src/index.ts
@@ -3,4 +3,5 @@
 export * from './aqua-protocol-contract'
 export * as ABI from './abi'
 
-export { Address, HexString, NetworkEnum, CallInfo } from '@1inch/sdk-core'
+export { Address, HexString, NetworkEnum } from '@1inch/sdk-core'
+export type { CallInfo } from '@1inch/sdk-core'
